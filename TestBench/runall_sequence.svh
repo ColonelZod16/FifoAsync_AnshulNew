aa614ec0df78abe3fdfd9db4bcc6595bea5eb862
Anshul Patil <116883005+ColonelZod16@users.noreply.github.com>
2025-11-23 20:22:38 +0530

    Create runall_sequence.svh

---
+++ TestBench/runall_sequence.svh
@@ -0,0 +1,18 @@
+class runall_sequence extends uvm_sequence #(uvm_sequence_item);
+  `uvm_object_utils(runall_sequence)
+
+  protected random_sequence random;
+  protected sequencer sequencer_h;
+  protected uvm_component uvm_component_h;
+
+  function new(string name = "runall_sequence");
+    super.new(name);
+    uvm_component_h = uvm_top.find("*.env_h.sequencer_h");
+    $cast(sequencer_h, uvm_component_h);
+    random = random_sequence::type_id::create("random");
+  endfunction
+
+  task body();
+    random.start(sequencer_h);
+  endtask
+endclass
