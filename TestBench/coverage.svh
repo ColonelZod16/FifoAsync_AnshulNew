177875d3fbf5ab29d70352434aafda75a1894515
Anshul Patil <116883005+ColonelZod16@users.noreply.github.com>
2026-01-15 16:17:20 +0530

    Update coverage.svh

--- TestBench/coverage.svh
+++ TestBench/coverage.svh
@@ -9,7 +9,7 @@ class coverage extends uvm_subscriber #(sequence_item);
       bins others = {['h01:'hFE]};
       bins ones = {'hFF};
     }
-  endgroup
+  endgroup    //connected to input monitor's ap port by analysis export so write(() is called automatically to check for coverage
 
   function new (string name, uvm_component parent);
     super.new(name, parent);
@@ -17,7 +17,7 @@ class coverage extends uvm_subscriber #(sequence_item);
   endfunction
 
   function void write(sequence_item t);
-    wdata = t.wdata;
+    wdata = t.wdata;   //Coverage sampling is triggered when a valid transaction occurs; since the BFM already synchronizes to the correct simulation time, calling sample() immediately inside the subscriber’s write() method is correct and race-free.”
     wdata_cov.sample();
   endfunction
 endclass
