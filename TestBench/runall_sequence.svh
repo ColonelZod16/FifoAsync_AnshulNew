dc12d32dc0e51ccbf286a05d1e5f285026e5cf81
Anshul Patil <116883005+ColonelZod16@users.noreply.github.com>
2026-01-15 15:40:07 +0530

    Update runall_sequence.svh

--- TestBench/runall_sequence.svh
+++ TestBench/runall_sequence.svh
@@ -2,17 +2,17 @@ class runall_sequence extends uvm_sequence #(uvm_sequence_item);
   `uvm_object_utils(runall_sequence)
 
 random_sequence random;
- sequencer sequencer_h;
- uvm_component uvm_component_h;
+ sequencer sequencer_h;    //create a random sequencer_h
+ uvm_component uvm_component_h;  //create a generic handle
 
   function new(string name = "runall_sequence");
     super.new(name);
-    uvm_component_h = uvm_top.find("*.env_h.sequencer_h");
-    $cast(sequencer_h, uvm_component_h);
-    random = random_sequence::type_id::create("random");
+    uvm_component_h = uvm_top.find("*.env_h.sequencer_h");  //find the existing sequencer defined in env.sv 
+    $cast(sequencer_h, uvm_component_h);  //typecast the component_h which contains sequencer to the sequncer_h type which can now starting sending sequences
+    random = random_sequence::type_id::create("random");   //create a random sequence object
   endfunction
 
   task body();
-    random.start(sequencer_h);
+    random.start(sequencer_h);   //starts sending random sequences through the sequencer
   endtask
 endclass
