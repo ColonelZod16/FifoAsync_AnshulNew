3dcbbab2e62ba2359bbdc6dd177983e0b630624e
Anshul Patil <116883005+ColonelZod16@users.noreply.github.com>
2026-01-15 15:51:50 +0530

    Improve input flow monitor implementation
    Refactor input flow monitor for clarity and functionality.

--- TestBench/input_flow_monitor.svh
+++ TestBench/input_flow_monitor.svh
@@ -9,19 +9,21 @@ class input_flow_monitor extends uvm_component;
   endfunction
 
   function void build_phase(uvm_phase phase);
-    if(!uvm_config_db #(virtual async_fifo_bfm)::get(null, "*", "bfm", bfm))
+    if(!uvm_config_db #(virtual async_fifo_bfm)::get(null, "*", "bfm", bfm))   //registers itself with BFM
       `uvm_fatal("DRIVER", "Failed to get BFM")
-    ap = new("ap", this);
+      ap = new("ap", this);   //creata an anaylsis port
   endfunction
 
   function void connect_phase(uvm_phase phase);
-    bfm.input_flow_monitor_h = this;
+    bfm.input_flow_monitor_h = this;    //connects to BFM
   endfunction
 
   function void write_to_monitor(logic [FIFO_DATA_WIDTH-1:0] wdata);
-    sequence_item push;
+    sequence_item push;    //BFM calls this function to write the data to the ap
     push = new("push");
-    push.wdata = wdata;
-    ap.write(push);
-  endfunction
+    push.wdata = wdata;   //copying the data received through BFM.push() to the monitors data
+    ap.write(push);   //Monitor now broadcasts this where ever required
+  endfunction    //Monitors do not “receive broadcasts”.
+//Monitors are the broadcasters.
+//Consumers (scoreboard, coverage) receive the broadcast.
 endclass
