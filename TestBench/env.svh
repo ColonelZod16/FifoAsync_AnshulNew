0df109e93461bba80b8bdec40b6fb80b853d2d33
Anshul Patil <116883005+ColonelZod16@users.noreply.github.com>
2026-01-15 15:00:03 +0530

    Enhance comments in env.svh for better understanding
    Updated comments for clarity and added explanations for component creation and connections.

--- TestBench/env.svh
+++ TestBench/env.svh
@@ -1,9 +1,9 @@
 class env extends uvm_env;
-  `uvm_component_utils(env)
+  `uvm_component_utils(env)      //registering with uvm_factory to use type_id::create()
 
   sequencer           sequencer_h;
   coverage            coverage_h;
-  scoreboard          scoreboard_h;
+  scoreboard          scoreboard_h;     
   driver              driver_h;
   input_flow_monitor  input_flow_monitor_h;
   output_flow_monitor output_flow_monitor_h;
@@ -13,7 +13,7 @@ class env extends uvm_env;
   endfunction
 
   function void build_phase(uvm_phase phase);
-    sequencer_h              = new("sequencer_h", this);
+    sequencer_h              = new("sequencer_h", this);   //this is not factory based , cannot be overidden  “Factory creation allows component replacement without modifying env code.”
     driver_h                 = driver::type_id::create("driver_h", this);
     input_flow_monitor_h     = input_flow_monitor::type_id::create("input_flow_monitor_h", this);
     output_flow_monitor_h    = output_flow_monitor::type_id::create("output_flow_monitor", this);
@@ -22,9 +22,9 @@ class env extends uvm_env;
   endfunction
 
   function void connect_phase(uvm_phase phase);
-    driver_h.seq_item_port.connect(sequencer_h.seq_item_export);
-    input_flow_monitor_h.ap.connect(coverage_h.analysis_export);
-    input_flow_monitor_h.ap.connect(scoreboard_h.input_flow_f.analysis_export);
-    output_flow_monitor_h.ap.connect(scoreboard_h.analysis_export);
+    driver_h.seq_item_port.connect(sequencer_h.seq_item_export);   //driver can now pull data from the sequencer
+    input_flow_monitor_h.ap.connect(coverage_h.analysis_export);   //broadcasting to coverage
+    input_flow_monitor_h.ap.connect(scoreboard_h.input_flow_f.analysis_export);  //ap supports many to one so sending to scoreboard 
+    output_flow_monitor_h.ap.connect(scoreboard_h.analysis_export); //sending to scoreboard to compare input data with output data
   endfunction
-endclass
+endclass   //all above are TLM ports
