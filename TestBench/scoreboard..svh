e02f2ff4e16e95544d3ba8a9382be931343ef919
Anshul Patil <116883005+ColonelZod16@users.noreply.github.com>
2026-01-15 16:13:52 +0530

    Update scoreboard..svh

--- TestBench/scoreboard..svh
+++ TestBench/scoreboard..svh
@@ -11,27 +11,27 @@ class scoreboard extends uvm_subscriber #(output_flow_transaction);
     input_flow_f = new ("input_flow_f", this);
   endfunction
 
-  function output_flow_transaction expected_output_flow(sequence_item input_flow);
+  function output_flow_transaction expected_output_flow(sequence_item input_flow);    //converts the received input transaction to output transaction type by copying the input data to the "expected" object
     output_flow_transaction expected;
     expected = new("expected");
     expected.rdata = input_flow.wdata;
     return expected;
   endfunction
 
-  virtual function void write(output_flow_transaction t);
+  virtual function void write(output_flow_transaction t);   //output monitor calls the ap.write() , ap is connected to tlm port of this scoreboard which extended by uvm_subscriber so this write() is called , the analysis export is wired to the write() function , so it gets the output transaction
     sequence_item input_flow;
     output_flow_transaction expected;
 
-    input_flow_f.try_get(input_flow);
-    expected = expected_output_flow(input_flow);
+    input_flow_f.try_get(input_flow);    // the input monitor's ap is connected to the input_flow_f so we use try_get() to get the item.
+    expected = expected_output_flow(input_flow);  //getting the expected data item from the input
     data_str = {                     input_flow.convert2string(), 
-                  " ==>  Actual "  , t.convert2string(), 
+                " ==>  Actual "  , t.convert2string(),    
                   "/expected "     , expected.convert2string()};
 
     if (!expected.compare(t))
       `uvm_error("SCOREBOARD SELF CHECKER", {"FAIL: ", data_str})
     else
-      `uvm_info ("SCOREBOARD SELF CHECKER", {"PASS: ", data_str}, UVM_LOW)
+      `uvm_info ("SCOREBOARD SELF CHECKER", {"PASS: ", data_str}, UVM_LOW)   //checking if they are the same or not
 
   endfunction
 endclass
