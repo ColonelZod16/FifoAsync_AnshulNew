5e8274f2894878ba7d06562c962314070f5bfe3e
Anshul Patil <116883005+ColonelZod16@users.noreply.github.com>
2026-01-15 15:22:22 +0530

    Enhance driver class with better comments and logic
    Updated comments for clarity and added checks for handling the last item in push and pop tasks.

--- TestBench/driver.svh
+++ TestBench/driver.svh
@@ -1,32 +1,32 @@
 class driver extends uvm_driver #(sequence_item);
   `uvm_component_utils(driver)
 
-  virtual async_fifo_bfm bfm;
+  virtual async_fifo_bfm bfm;    ///virtual because it gets the handle to an existing interface instance , enables loose coupling between TB and DUT
 
   function new (string name, uvm_component parent);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    if(!uvm_config_db #(virtual async_fifo_bfm)::get(null, "*", "bfm", bfm))
+    if(!uvm_config_db #(virtual async_fifo_bfm)::get(null, "*", "bfm", bfm))   //fetches the interface from config db defined in top module
       `uvm_fatal("DRIVER", "Failed to get BFM")
   endfunction
 
   task push();
-    sequence_item push;
+    sequence_item push;    
     integer i = 0;
-    forever begin
-      if (i < TEST_FLOW_LENGTH-2) begin
-        seq_item_port.get_next_item(push);
+    forever begin 
+      if (i < TEST_FLOW_LENGTH-2) begin   //checking for the last item to send the last signal as well so bfm can handle that case as well
+        seq_item_port.get_next_item(push);   //blocking call to get the next item from sequence
         bfm.push(push.wdata, 1'b0);
         seq_item_port.item_done();
       end else begin
         seq_item_port.get_next_item(push);
-        bfm.push(push.wdata, 1'b1);
+        bfm.push(push.wdata, 1'b1);   //last signal and last item sent
         seq_item_port.item_done();
       end
       i++;
-      if (i > TEST_FLOW_LENGTH-1) i = 0;
+      if (i > TEST_FLOW_LENGTH-1) i = 0; //restarting the burst counter
     end
   endtask
 
@@ -34,18 +34,18 @@ class driver extends uvm_driver #(sequence_item);
     integer i = 0;
     forever begin
       if (i < TEST_FLOW_LENGTH-2)
-        bfm.pop(1'b0);
+        bfm.pop(1'b0);   //similar logic here
       else
-        bfm.pop(1'b1);
+        bfm.pop(1'b1);   //last signal
       i++;
-      if (i > TEST_FLOW_LENGTH-1) i = 0;
+      if (i > TEST_FLOW_LENGTH-1) i = 0;   //restarting the read counter
     end
   endtask
 
   task run_phase(uvm_phase phase);
-    bfm.reset_rdwr();
+    bfm.reset_rdwr();   //resetting the FIFO to a known state
     fork
-      push();
+      push();       
       pop();
     join_none
   endtask
